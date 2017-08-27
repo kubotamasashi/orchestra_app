c78d5d689b9afeb2eff3ec809f29d36fbfd63c80
kubota masashi <jyvina@gmail.com>
2017-08-27 15:20:28 +0000

    Fnish user registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 OrchestraApp::Application.routes.draw do
   
   root "static_pages#home"
-  devise_for :users
+  devise_for :users, :controllers => {
+    :registrations => "registrations"
+  }
   resources :articles 
   resources :users, only: [:show]
   get "static_pages/introduction"
